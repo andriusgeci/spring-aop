f53955de5d5623d0dcdf9ed850b76bf022c86b82
AndriusG <ageciauskas@digisoft.tv>
2018-06-05 20:09:25 +0100

    spring-aop

---
+++ src/main/java/com/in28minutes/spring/aop/springaop/aspect/UserAccessAspect.java
@@ -0,0 +1,25 @@
+package com.in28minutes.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+//aop
+//configuration
+@Aspect
+@Configuration
+public class UserAccessAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    //what kid of method calls I would intercept
+    //execution(* PACKAGE.*.*(..))
+    @Before("execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))")
+    public void before(JoinPoint joinPoint){
+        logger.info("Check for user access");
+        logger.info("Allowed execution for {}", joinPoint);
+    }
+}
---
+++ src/main/java/com/in28minutes/spring/aop/springaop/business/Business1.java
@@ -0,0 +1,17 @@
+package com.in28minutes.spring.aop.springaop.business;
+
+import com.in28minutes.spring.aop.springaop.data.Dao1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Business1 {
+
+    @Autowired
+    private Dao1 dao1;
+
+    public String calculateSomething(){
+        //com.in28minutes.spring.aop.springaop.business logic
+        return dao1.retrieveSomething();
+    }
+}
